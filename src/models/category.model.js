dd83e826f6383433058c7e224620bd1697d3d878
Hà Minh Nhật <131330599+bsv-nhatha@users.noreply.github.com>
2024-07-19 22:00:35 +0700

    check data error

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
 
 const { Schema } = mongoose;
 
@@ -6,7 +7,14 @@ const categorySchema = new Schema({
   name: { type: String, required: true, unique: true },
   idCategory: [{ type: Schema.Types.ObjectId, ref: 'SubCategory', required: true }],
 });
-
+// add plugin that converts mongoose to json
+categorySchema.plugin(toJSON);
+categorySchema.plugin(paginate);
+categorySchema.statics.isNameTaken = async function (name, excludeCategoryId) {
+  const category = await this.findOne({ name, _id: { $ne: excludeCategoryId } });
+  return !!category;
+};
+categorySchema.index({ name: 1 });
 const Category = mongoose.model('Category', categorySchema);
 
 module.exports = Category;
